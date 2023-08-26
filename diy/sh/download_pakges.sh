3d02223e60b87de689356f9189cf8e6043d6a9a8
huismiling <zh_smiling@yeah.net>
2023-08-26 19:06:59 +0800

    fix build dir

---
+++ diy/sh/download_pakges.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+
+wget https://github.com/openwrt/openwrt/archive/refs/tags/v22.03.5.zip
+
+
+
--- diy/sh/imagebuild.sh
+++ diy/sh/imagebuild.sh
@@ -3,12 +3,13 @@ set -x
 echo $USER
 echo $PWD
 ls -l 
-chown -R 1000:1000 /home/build
+chown -R 1000:1000 /tmp/build
 
 # 添加签名验证
 chmod +x tool/usign
 cp tool/public.key keys/$(./tool/usign -F -p tool/public.key)
-mkdir -p files/etc/opkg/keys && cp tool/public.key files/etc/opkg/keys/$(./tool/usign -F -p tool/public.key)
+mkdir -p files/etc/opkg/keys 
+cp tool/public.key files/etc/opkg/keys/$(./tool/usign -F -p tool/public.key)
 
 bash target.sh
 
