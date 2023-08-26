5f15c80920685b61da3c1abe2ead3d303273ce79
huismiling <zh_smiling@yeah.net>
2023-08-27 00:32:57 +0800

    up

--- diy/sh/download_packages.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-
-wget https://github.com/openwrt/openwrt/archive/refs/tags/v22.03.5.zip
-
-
-
--- diy/sh/imagebuild.sh
+++ diy/sh/imagebuild.sh
@@ -3,15 +3,6 @@ set -x
 echo $USER
 echo $PWD
 ls -l 
-chown -R 1000:1000 /tmp/build
-
-# 添加签名验证
-chmod +x tool/usign
-cp tool/public.key keys/$(./tool/usign -F -p tool/public.key)
-mkdir -p files/etc/opkg/keys 
-cp tool/public.key files/etc/opkg/keys/$(./tool/usign -F -p tool/public.key)
 
 bash target.sh
-
-
 
